6b76bc51972a8a149dfa3d676852626cb0f3650c
tianhe1986 <w1s2j3229@163.com>
2019-08-06 14:44:51 +0800

    修复时间单位。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- RewardedVideo/RewardedVideoConfig.ts
+++ RewardedVideo/RewardedVideoConfig.ts
@@ -5,6 +5,6 @@ module tianhe1986{
 	export class RewardedVideoConfig{
 		
 		public static videoId:string = ""; // 因为是全局单例，只需要一个id即可
-		public static reloadInterval: number = 60; // 当拉取广告失败时， 自动尝试重新拉取的间隔时间，默认1分钟
+		public static reloadInterval: number = 60 * 1000; // 当拉取广告失败时， 自动尝试重新拉取的间隔时间，默认1分钟
 	}
 }
