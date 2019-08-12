02379aeffa550c6e7c602f728922bc5e95ceba5d
tianhe1986 <w1s2j3229@163.com>
2019-08-12 10:31:47 +0800

    web处理。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

---
+++ RewardedVideo/Impl/RewardedVideoImplWeb.ts
@@ -0,0 +1,26 @@
+/**
+* name 
+*/
+module FatLayaHelper{
+	export class RewardedVideoImplWeb implements RewardedVideoImpl{
+		constructor(){
+
+		}
+
+		public create(videoId:string):void
+		{
+			// 根本创建不了一郎
+		}
+
+		public load():void
+		{
+			// 没有事情做
+		}
+
+		public show(needLoad: boolean, errorHandler: Laya.Handler):void
+		{
+			// 直接失败就好
+			RewardedVideoManager.instance.playFailedsHandler && RewardedVideoManager.instance.playFailedsHandler.run();
+		}
+	}
+}
