6adb4cf73793a0809c36dec8fd9616ac004c6114
tianhe1986 <w1s2j3229@163.com>
2019-08-21 10:39:48 +0800

    增加微信平台处理
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- InterstitialAd/InterstitialAdManager.ts
+++ InterstitialAd/InterstitialAdManager.ts
@@ -7,14 +7,17 @@ module FatLayaHelper{
 		private static _instance: InterstitialAdManager = null;
 
 		//全局展示间隔
-		private interShowGlobalKey:string = "fat_laya_inter_ad_all_time";
+		private _interShowGlobalKey:string = "fat_laya_inter_ad_all_time";
 
 		//单个广告下次展示时间
-		private interShowSingleKeyPrefix:string = "fat_laya_inter_ad_single_time_";
+		private _interShowSingleKeyPrefix:string = "fat_laya_inter_ad_single_time_";
 
 		//下次展示时间戳
-		private interShowGlobalNextTime:number = null;
-		private interShowSingleNextTimeMap: Object = {};
+		private _interShowGlobalNextTime:number = null;
+		private _interShowSingleNextTimeMap: Object = {};
+
+		//缓存的处理实例
+		private _implInstanceMap:Object = {};
 
 		public static get instance() {
 			if (! this._instance) {
@@ -33,6 +36,11 @@ module FatLayaHelper{
 		{
 			// TODO 微信小游戏， 可以将已创建的实例缓存起来
 			switch (GlobalConfig.platform) {
+				case Platform.WEIXIN:
+					if (this._implInstanceMap[adId] == undefined) {
+						this._implInstanceMap[adId] = new InterstitialAdImplWx(adId);
+					}
+					return this._implInstanceMap[adId];
 				case Platform.VIVO: //vivo小游戏， 根据文档，如果用户关闭了插屏广告，则需要重新创建，因此缓存起来没有意义，每次都返回一个新实例
 					return new InterstitialAdImplVivo(adId);
 				default:
@@ -68,16 +76,16 @@ module FatLayaHelper{
 				return 0;
 			}
 
-			if (this.interShowGlobalNextTime === null) { //没有的情况下， 从缓存中取
-				let valueStr = Laya.LocalStorage.getItem(this.interShowGlobalKey);
+			if (this._interShowGlobalNextTime === null) { //没有的情况下， 从缓存中取
+				let valueStr = Laya.LocalStorage.getItem(this._interShowGlobalKey);
 				if (valueStr == null || valueStr == undefined || valueStr == "") {
-					this.interShowGlobalNextTime = 0;
+					this._interShowGlobalNextTime = 0;
 				} else {
-					this.interShowGlobalNextTime = parseFloat(valueStr);
+					this._interShowGlobalNextTime = parseFloat(valueStr);
 				}
 			}
 
-			return this.interShowGlobalNextTime;
+			return this._interShowGlobalNextTime;
 		}
 
 		// 获取单个id下次展示时间
@@ -87,16 +95,16 @@ module FatLayaHelper{
 				return 0;
 			}
 
-			if (this.interShowSingleNextTimeMap[adId] == undefined) {
-				let valueStr = Laya.LocalStorage.getItem(this.interShowSingleKeyPrefix + adId);
+			if (this._interShowSingleNextTimeMap[adId] == undefined) {
+				let valueStr = Laya.LocalStorage.getItem(this._interShowSingleKeyPrefix + adId);
 				if (valueStr == null || valueStr == undefined || valueStr == "") {
-					this.interShowSingleNextTimeMap[adId] = 0;
+					this._interShowSingleNextTimeMap[adId] = 0;
 				} else {
-					this.interShowSingleNextTimeMap[adId] = parseFloat(valueStr);
+					this._interShowSingleNextTimeMap[adId] = parseFloat(valueStr);
 				}
 			}
 
-			return this.interShowSingleNextTimeMap[adId];
+			return this._interShowSingleNextTimeMap[adId];
 		}
 
 		// 更新插屏广告展示时间
@@ -105,13 +113,13 @@ module FatLayaHelper{
 			let timeNow:number = Date.now();
 
 			if (InterstitialAdConfig.singleInterval > 0) {
-				this.interShowSingleNextTimeMap[adId] = timeNow + InterstitialAdConfig.singleInterval;
-				Laya.LocalStorage.setItem(this.interShowSingleKeyPrefix + adId, "" + this.interShowSingleNextTimeMap[adId]);
+				this._interShowSingleNextTimeMap[adId] = timeNow + InterstitialAdConfig.singleInterval;
+				Laya.LocalStorage.setItem(this._interShowSingleKeyPrefix + adId, "" + this._interShowSingleNextTimeMap[adId]);
 			}
 
 			if (InterstitialAdConfig.globalInterval > 0) {
-				this.interShowGlobalNextTime = timeNow + InterstitialAdConfig.globalInterval;
-				Laya.LocalStorage.setItem(this.interShowGlobalKey, "" + this.interShowGlobalNextTime);
+				this._interShowGlobalNextTime = timeNow + InterstitialAdConfig.globalInterval;
+				Laya.LocalStorage.setItem(this._interShowGlobalKey, "" + this._interShowGlobalNextTime);
 			}
 		}
 	}
