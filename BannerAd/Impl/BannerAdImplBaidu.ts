7b644069027cb16e849f4e4d022386851e0f8ed4
tianhe1986 <w1s2j3229@163.com>
2019-09-17 15:26:16 +0800

    百度banner处理。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- BannerAd/Impl/BannerAdImplBaidu.ts
+++ BannerAd/Impl/BannerAdImplBaidu.ts
@@ -7,8 +7,14 @@ module FatLayaHelper{
 
 		constructor(adId:string, style:Object) {
 			// 宽高比默认是 3：1， 设置高度也没用
-			let height:number = style["width"] / 3;
-			let left:number = style["left"] ? style["left"] : (BannerAdManager.instance.systemInfo.systemWidth - style["width"])/2;
+			let width:number = style["width"];
+			if (width < 300) {
+				width = 300;
+			} else if (width > BannerAdManager.instance.systemInfo.systemWidth) {
+				width = BannerAdManager.instance.systemInfo.systemWidth;
+			}
+			let height:number = width / 3;
+			let left:number = style["left"] ? style["left"] : (BannerAdManager.instance.systemInfo.systemWidth - width)/2;
 			let top:number = style["top"] ? style["top"] : (BannerAdManager.instance.systemInfo.systemHeight - height);
 
 			this._bannerAd = Laya.Browser.window.swan.createBannerAd({
@@ -17,12 +23,15 @@ module FatLayaHelper{
 				style: {
 					left: left,
 					top: top,
-					width: style["width"]
+					width: width
 				}
 			});
 
 			this._bannerAd.onResize((res) => {
-				//TODO 
+				let left:number = style["left"] ? style["left"] : (BannerAdManager.instance.systemInfo.systemWidth - res.width)/2;
+				let top:number = style["top"] ? style["top"] : (BannerAdManager.instance.systemInfo.systemHeight - res.height);
+				this._bannerAd.style.left = left;
+				this._bannerAd.style.top = top;
 			});
 		}
 
